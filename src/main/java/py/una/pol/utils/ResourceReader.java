22bad371c505cde23d54bde9f40603d069fca3d3
elviopy <elvio07velazquez@gmail.com>
2022-05-04 22:50:12 -0400

    topologia y algunas clases

---
+++ src/main/java/py/una/pol/utils/ResourceReader.java
@@ -0,0 +1,22 @@
+package py.una.pol.utils;
+
+
+import java.io.InputStream;
+
+public class ResourceReader {
+
+    public static InputStream getFileFromResourceAsStream(String fileName) {
+
+        // The class loader that loaded the class
+        ClassLoader classLoader = ResourceReader.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        // the stream holding the file content
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+
+    }
+}
