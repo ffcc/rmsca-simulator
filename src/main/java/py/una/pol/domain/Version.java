4c006708ce746e9a8e60ce309ed2a4ca1ce23b42
ffcc <fercaba01@gmail.com>
2025-10-04 12:41:25 -0300

    update: Versión del proyecto al 1.3.0

--- src/main/java/py/una/pol/domain/Version.java
+++ src/main/java/py/una/pol/domain/Version.java
@@ -14,11 +14,14 @@ public class Version {
     public static final Version ORIGINAL_FIX = new Version("1.0.1", "Correcciones a la implementación original");
     public static final Version ORIGINAL_FIX_MODULATION = new Version("1.0.3", "Corrección del cálculo " +
             "de la modulación basado en la distancia de cada KSP");
+
     public static final Version ORDERED_CORE_BFR = new Version("1.2.0", "Recorrido de los cores ordenado por BFR");
     public static final Version ORDERED_CORE_BFR_PERIPHERAL = new Version("1.2.1", "Recorrido de los cores con los periféricos ordenado por BFR");
     public static final Version ORDERED_CORE_BFR_EVEN_ODD = new Version("1.2.2", "Recorrido de los cores con los periféricos pares primero");
     public static final Version ORDERED_CORE_BFR_PERIPHERAL_FIX = new Version("1.2.3", "1.0.2 + 1.2.1");
     public static final Version ORDERED_CORE_BFR_EVEN_ODD_FIX = new Version("1.2.4", "1.0.2 + 1.2.2");
+
+    public static final Version ORDERED_CORE_BFR_PERIPHERAL_FIX_MODULATION = new Version("1.3.0", "Recorrido por BFR + Fix modulation KSP (1.0.3 + 1.2.3)");
 
     private Version(String value, String description) {
         this.value = value;
